459075d916496d9da50c55e4dd58dcba69ee0c36
Nick Penzenstadler <penzenstadler@gmail.com>
2016-03-23 14:27:41 -0600

    Third try. 2:27 p.m. Wed.

--- homework0322.sql
+++ homework0322.sql
@@ -1,6 +1,6 @@
 --homework
 
-SELECT CONCAT (c.first_name,' ', c.last_name) AS "Name",
+		CONCAT (c.first_name,' ', c.last_name) AS "Name1",
         s.school_name AS "School",
 		s.school_id AS "ID",
 		st.state_name AS "State",
@@ -13,16 +13,14 @@ SELECT CONCAT (c.first_name,' ', c.last_name) AS "Name",
 		AVG (ct.total_pay) AS "Avg total pay"
 FROM sandbox.coach AS c
 INNER JOIN sandbox.contract AS ct
-		on ct.coach_id = c.id
+		on c.id = ct.coach_id
 INNER JOIN sandbox.school AS s
 		on s.school_id = ct.school_id
 INNER JOIN reference.state AS st
 		on s.state = st.state_abbreviation
 INNER JOIN sandbox.position AS pt
 		on pt.id = ct.position_id AND pt.id = 1
-LEFT OUTER JOIN sandbox.coach_record AS rd
-		on c.id = rd.coach_id 
 WHERE c.sport_id = 3
-GROUP BY c.last_name, c.first_name, s.school_name, s.school_id, st.state_name, ct.id, ct.school_pay, ct.other_pay, ct.total_pay
+GROUP BY c.first_name, c.last_name, s.school_name, s.school_id, st.state_name
 HAVING COUNT (ct.id) >= 3
 ORDER BY c.last_name, c.first_name;
